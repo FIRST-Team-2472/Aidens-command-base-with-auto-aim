713a584b41819c6626ae024ecfd639ed858acacc
ModernTinTin1 <sawyer.c.theis@gmail.com>
2023-10-17 16:47:37 -0500

    up to date

--- src/main/java/frc/commands/IntakeControls.java
+++ src/main/java/frc/commands/IntakeControls.java
@@ -6,13 +6,14 @@ import frc.robot.subsystems.Intake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class IntakeControls extends CommandBase{
-    private Supplier<Boolean> conveyorButton, elevatorButton, flywheelButton;
+    private Supplier<Boolean> conveyorButton, elevatorButton, flywheelButton, Reverse;
     private Intake Intake;
 
-    public IntakeControls(Supplier<Boolean> conveyorButton, Supplier<Boolean> elevatorButton, Supplier<Boolean> flywheelButton, Intake Intake) {
+    public IntakeControls(Supplier<Boolean> conveyorButton, Supplier<Boolean> elevatorButton, Supplier<Boolean> flywheelButton, Supplier<Boolean> Reverse, Intake Intake) {
         this.conveyorButton = conveyorButton;
         this.elevatorButton = elevatorButton;
         this.flywheelButton = flywheelButton;
+        this.Reverse = Reverse;
         this.Intake = Intake;
         addRequirements(Intake);
     }
@@ -22,18 +23,22 @@ public class IntakeControls extends CommandBase{
     public void execute() {
         if(flywheelButton.get())
             Intake.controlsFlywheel(-1);
-        else    
+        else if(Reverse.get()){   
+            Intake.controlsFlywheel(1);
+            Intake.controlsConveyor(-1);
+        }
+        else
             Intake.controlsFlywheel(0);
 
         if(conveyorButton.get())
             Intake.controlsConveyor(1);
-        else    
+        else if(Reverse.get())
+            Intake.controlsConveyor(-1);
+        else
             Intake.controlsConveyor(0);
 
         if(elevatorButton.get())
             Intake.controlsElevator(1);
-        else if(eject.get()) 
-        Intake.controlsElevator(-1);
         else
             Intake.controlsElevator(0);
     }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,8 +25,8 @@ public class RobotContainer {
 
     drivetrain.setDefaultCommand(new Drive(drivetrain, () -> leftJoy.getX(), () -> leftJoy.getY()));
     configureBindings();
-    turret.setDefaultCommand(new TurretControls(()-> rightJoy.getX(),  ()-> rightJoy.getY(), ()-> Xboxcontroller.getRightBumper(),()-> leftJoy.getRawButton(2), turret));
-    intake.setDefaultCommand(new IntakeControls(()-> rightJoy.getRawButton(2),()-> Xboxcontroller.getRightBumper(),() -> rightJoy.getRawButton(2), intake));
+    turret.setDefaultCommand(new TurretControls(()-> Xboxcontroller.getLeftX(),  ()-> Xboxcontroller.getLeftY(), ()-> Xboxcontroller.getRightBumper(),()-> Xboxcontroller.getLeftBumper(), turret));
+    intake.setDefaultCommand(new IntakeControls(()-> leftJoy.getRawButton(1),()-> Xboxcontroller.getRightBumper(),() -> leftJoy.getRawButton(1),() -> leftJoy.getRawButton(2), intake));
   }
 
   private void configureBindings() {}
